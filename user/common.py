eb6ef14f45c6541d9cee67291d0685480e5175a6
Administrator <956111647@qq.com>
2020-03-31 21:32:26 +0800

    phone password 登陆

---
+++ user/common.py
@@ -0,0 +1,31 @@
+from .models import User
+def user_is_exit(phone):
+    '''
+    判断用户是否存在，存在则返回该对象
+    不存在则返回空列表
+    '''
+    user_obj = User.objects.filter(phone=phone)
+    if user_obj:
+        return user_obj[0]
+    else:
+        return False
+
+
+def check_user_status(user_obj):
+    if user_obj.status == '1':
+        return True
+    else:
+        return False
+
+def chect_user_password(user_obj,password):
+    if user_obj.password == password:
+        return True
+    else:
+        return False
+
+def create_Token(user_obj):
+    import hashlib,time
+    ctime = str(time.time())
+    md5=hashlib.md5(bytes(user_obj.phone,encoding="utf8"))
+    md5.update(bytes(ctime,encoding="utf8"))
+    return md5.hexdigest()
--- user/views.py
+++ user/views.py
@@ -1,20 +1,19 @@
-from .models import User
+
 from django.core.cache import cache
 from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-
+from .common import user_is_exit,create_Token,chect_user_password,check_user_status
 # class phoneLoginSerializer(serializers.Serializer):
 #     phone = serializers.CharField(max_length=11, min_length=11, allow_blank=False, trim_whitespace=True)
 #     password = serializers.CharField(max_length=200, allow_blank=False, trim_whitespace=True)
-
 
 def message_response(code,messages,data={}):
     return Response({'code': code, 'messages': messages,'data':data})
 
 @api_view(['GET','POST'])
 def index(request):
-    Token = request.GET.get('Token',None)
+    Token = request.META.get('HTTP_TOKEN')
     if Token:
         user_obj = cache.get(Token,None)
         if user_obj:
@@ -44,33 +43,3 @@ def phonePasswordlogin(request):
             return message_response(20002,'用户手机不存在',{})
     return message_response(20003, '没有get', {})
 
-def user_is_exit(phone):
-    '''
-    判断用户是否存在，存在则返回该对象
-    不存在则返回空列表
-    '''
-    user_obj = User.objects.filter(phone=phone)
-    if user_obj:
-        return user_obj[0]
-    else:
-        return False
-
-
-def check_user_status(user_obj):
-    if user_obj.status == '1':
-        return True
-    else:
-        return False
-
-def chect_user_password(user_obj,password):
-    if user_obj.password == password:
-        return True
-    else:
-        return False
-
-def create_Token(user_obj):
-    import hashlib,time
-    ctime = str(time.time())
-    md5=hashlib.md5(bytes(user_obj.phone,encoding="utf8"))
-    md5.update(bytes(ctime,encoding="utf8"))
-    return md5.hexdigest()
