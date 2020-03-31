3b36ebe7d6e9a395fc889265152b0a421bfb1398
Administrator <956111647@qq.com>
2020-03-31 18:42:30 +0800

    phone password 登陆

--- user/views.py
+++ user/views.py
@@ -1,19 +1,12 @@
-from django.shortcuts import render
-from django.shortcuts import render,redirect,HttpResponse,reverse
-from django.http import JsonResponse
 from .models import User
-
-# Create your views here.
-from django.core import serializers
 from django.core.cache import cache
-
 from rest_framework import serializers
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-class phoneLoginSerializer(serializers.Serializer):
-    phone = serializers.CharField(max_length=11, min_length=11, allow_blank=False, trim_whitespace=True)
-    password = serializers.CharField(max_length=200, allow_blank=False, trim_whitespace=True)
+# class phoneLoginSerializer(serializers.Serializer):
+#     phone = serializers.CharField(max_length=11, min_length=11, allow_blank=False, trim_whitespace=True)
+#     password = serializers.CharField(max_length=200, allow_blank=False, trim_whitespace=True)
 
 
 def message_response(code,messages,data={}):
@@ -40,8 +33,8 @@ def phonePasswordlogin(request):
         if user_obj:
             if check_user_status(user_obj):
                 if chect_user_password(user_obj,password):
-                    Token = create_Token(phone)
-                    cache.set(Token,user_obj[0],60)
+                    Token = create_Token(user_obj)
+                    cache.set(Token,user_obj,60)
                     return message_response(20000, '登陆成功', {"Token":Token})
                 else:
                     return message_response(20004, '密码错误', {})
@@ -57,24 +50,27 @@ def user_is_exit(phone):
     不存在则返回空列表
     '''
     user_obj = User.objects.filter(phone=phone)
-    return user_obj
+    if user_obj:
+        return user_obj[0]
+    else:
+        return False
 
 
 def check_user_status(user_obj):
-    if user_obj[0].status == '1':
+    if user_obj.status == '1':
         return True
     else:
         return False
 
 def chect_user_password(user_obj,password):
-    if user_obj[0].password == password:
+    if user_obj.password == password:
         return True
     else:
         return False
 
-def create_Token(phone):
+def create_Token(user_obj):
     import hashlib,time
     ctime = str(time.time())
-    md5=hashlib.md5(bytes(phone,encoding="utf8"))
+    md5=hashlib.md5(bytes(user_obj.phone,encoding="utf8"))
     md5.update(bytes(ctime,encoding="utf8"))
     return md5.hexdigest()
